443234ced4f815c60440c9973a7ee5cb5f39eacc
angeldorcas92 <anyimigbochinwe@gmail.com>
2023-03-26 23:35:35 +0100

    router

--- hooks/src/Components/movielist/MovieList.jsx
+++ hooks/src/Components/movielist/MovieList.jsx
@@ -1,10 +1,12 @@
+
+
 import React from 'react';
 import MovieCard from '../moviecard/MovieCard';
 
 const MovieList = ({ movies }) => {
   return (
     <div className="movie-list">
-      {movies.map((movie) => (
+      {movies && movies.map((movie) => (
         <MovieCard key={movie.id} {...movie} />
       ))}
     </div>
